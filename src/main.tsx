f5f98d0677ecf9f1ea0206f563ba54d970f2933e
Robert (Lemon) Martinez-Newby <robert.mychal.martinez@gmail.com>
2024-09-09 10:37:17 -0400

    more fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import '@aws-amplify/ui-react/styles.css';
-import { ThemeProvider } from '@aws-amplify/ui-react';
-import { Amplify } from 'aws-amplify';
-import config from './aws-exports';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-Amplify.configure(config);
+import { Amplify } from 'aws-amplify'
+import config from './amplifyconfiguration.json'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+Amplify.configure(config)
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
